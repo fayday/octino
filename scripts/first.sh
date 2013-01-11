26753870a895603012ed5febbdc3807a941a4a0f
fayday <frosmic@gmail.com>
2013-01-11 20:24:49 +0100

    Initiate repo

---
+++ scripts/first.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+case $1 in
+  dupa ) 
+    echo "Co to za bluzgi??!!"
+    exit 1 ;;
+  oxy )
+    echo "Poród za 30 min!" ;;
+  milka )
+    echo "Czekolada." ;;
+  cola )
+    echo "Napój." ;;
+  * )
+    echo "Co to kurwa za pajaki są" ;;
+esac
+
+

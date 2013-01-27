be65e6dd0f09e661799980459386a42d5d8cd6a8
fayday <frosmic@gmail.com>
2013-01-27 18:09:38 +0100

    User module

--- scripts/user.sh
+++ scripts/user.sh
@@ -1,8 +1,20 @@
 #! /bin/bash
 
+if ! [[ "$1" =~ ^[0-9]+$ ]]; then
+  echo "ID must be numeric!"
+  exit
+fi
+
 for i in $(cut -f 1,3 -d: /etc/passwd); do
   array[${i#*:}]=${i%:*}
+  if [ "${i#*:}" -eq "$1" ]; then 
+    exist=true
+  fi
 done
 
-echo "User ID $1 is ${array[$1]}."
-echo "There are currently ${#array[@]} user accounts on the system."
+if [ -z $exist ]; then
+  echo "Selected ID doesn't exist!"
+else
+  echo "User ID $1 is ${array[$1]}."
+  echo "There are currently ${#array[@]} user accounts on the system."
+fi

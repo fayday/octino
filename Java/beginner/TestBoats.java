30fd07f3a3d9a66cb0798a4c29f5953209867567
fayday <frosmic@gmail.com>
2013-03-01 21:42:38 +0100

    Polymorphism

---
+++ Java/beginner/TestBoats.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class TestBoats {
+  public static void main(String[] args) {
+    Boat b1 = new Boat();
+    Sailboat b2 = new Sailboat();
+    Rowboat b3 = new Rowboat();
+    b2.setLength(32);
+    b1.move();
+    b3.move();
+    b2.move();
+
+    // rubbish - just to test ArrayList<Object>
+    ArrayList<Sailboat> blist = new ArrayList<Sailboat>();
+    Sailboat b4 = new Sailboat();
+    blist.add(b4);
+    Sailboat b5 = blist.get(0);
+  }
+:
+}
